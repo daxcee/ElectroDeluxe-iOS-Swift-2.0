a45510e3238116531517ca214549744b1d187bc1
srmds <stevenramdas@gmail.com>
2015-08-21 18:32:47 +0200

    Initial Commit

---
+++ ElectroDeluxe-Swift2.0Tests/ElectroDeluxe_Swift2_0Tests.swift
@@ -0,0 +1,35 @@
+//
+//  ElectroDeluxe_Swift2_0Tests.swift
+//  ElectroDeluxe-Swift2.0Tests
+//
+//  Created by c0d3r on 21/08/15.
+//  Copyright © 2015 srmds. All rights reserved.
+//
+
+import XCTest
+
+class ElectroDeluxe_Swift2_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
